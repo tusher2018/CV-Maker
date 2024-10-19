d31888c792a0e4c5ff99d04879afbcca4899be21
shishir2018 <www.shishir.symphony@gmail.com>
2024-10-19 23:10:21 +0600

    CV maker only

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
-// ignore_for_file: must_be_immutable, use_build_context_synchronously
-import 'dart:io';
+// ignore_for_file: must_be_immutable
+
 import 'dart:typed_data';
 
 import 'package:camscanner/CV%20templets/CV_templets.dart';
-import 'package:camscanner/CreateNID.dart';
+
 import 'package:camscanner/common_widget.dart';
-import 'package:camscanner/createPdf.dart';
-import 'package:camscanner/mergePdf.dart';
-import 'package:camscanner/removebd.dart';
+
 import 'package:camscanner/splash_page.dart';
-import 'package:camscanner/text_Recognaization.dart';
-import 'package:google_mlkit_document_scanner/google_mlkit_document_scanner.dart';
-import 'package:image/image.dart' as img;
+
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:pdf/pdf.dart';
@@ -47,6 +43,8 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   List<Uint8List> uint8list = [];
+
+  MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -63,187 +61,6 @@ class MyHomePage extends StatelessWidget {
               crossAxisCount: 3,
               crossAxisSpacing: 10,
               children: <Widget>[
-                iconButtonWithText(
-                  icon: "assets/imges/id.png",
-                  text: 'Create NID Brighter',
-                  function: () async {
-                    Uint8List? font, back;
-                    await DocumentScanner(
-                      options: DocumentScannerOptions(
-                        documentFormat:
-                            DocumentFormat.jpeg, // set output document format
-                        mode: ScannerMode
-                            .filter, // to control what features are enabled
-                        pageLimit:
-                            2, // setting a limit to the number of pages scanned
-                        isGalleryImport:
-                            true, // importing from the photo gallery
-                      ),
-                    ).scanDocument().then((value) async {
-                      for (var picture in value.images) {
-                        if (font == null) {
-                          font = await File(picture).readAsBytes();
-                          final img.Image image = img.decodeImage(font!)!;
-                          img.adjustColor(
-                            image,
-                            brightness: 1.2,
-                          );
-                          font = Uint8List.fromList(img.encodeJpg(image));
-                        } else {
-                          back = await File(picture).readAsBytes();
-                          final img.Image image = img.decodeImage(back!)!;
-                          img.adjustColor(
-                            image,
-                            brightness: 1.2,
-                          );
-                          back = Uint8List.fromList(img.encodeJpg(image));
-                        }
-                      }
-                      if (font == null && back == null) {
-                        return;
-                      }
-
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        if (font != null && back != null) {
-                          return CreateNID(
-                              isCamera: true,
-                              nidBackImage: back,
-                              nidFontImage: font);
-                        } else {
-                          return CreateNID(
-                              isCamera: true,
-                              nidBackImage: null,
-                              nidFontImage: font);
-                        }
-                      }));
-                    });
-                    uint8list = [];
-                  },
-                ),
-                iconButtonWithText(
-                  icon: "assets/imges/id.png",
-                  text: 'Create NID',
-                  function: () async {
-                    Uint8List? font, back;
-                    await DocumentScanner(
-                      options: DocumentScannerOptions(
-                        documentFormat:
-                            DocumentFormat.jpeg, // set output document format
-                        mode: ScannerMode
-                            .filter, // to control what features are enabled
-                        pageLimit:
-                            2, // setting a limit to the number of pages scanned
-                        isGalleryImport:
-                            true, // importing from the photo gallery
-                      ),
-                    ).scanDocument().then((value) async {
-                      for (var picture in value.images) {
-                        if (font == null) {
-                          font = await File(picture).readAsBytes();
-                        } else {
-                          back = await File(picture).readAsBytes();
-                        }
-                      }
-
-                      if (font == null && back == null) {
-                        return;
-                      }
-
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        if (font != null && back != null) {
-                          return CreateNID(
-                              isCamera: true,
-                              nidBackImage: back,
-                              nidFontImage: font);
-                        } else {
-                          return CreateNID(
-                              isCamera: true,
-                              nidBackImage: null,
-                              nidFontImage: font);
-                        }
-                      }));
-                    });
-                    uint8list = [];
-                  },
-                ),
-                iconButtonWithText(
-                  icon: "assets/imges/pdf.png",
-                  text: 'Create PDF',
-                  function: () async {
-                    await DocumentScanner(
-                      options: DocumentScannerOptions(
-                        documentFormat:
-                            DocumentFormat.jpeg, // set output document format
-                        mode: ScannerMode
-                            .filter, // to control what features are enabled
-                        pageLimit:
-                            10000, // setting a limit to the number of pages scanned
-                        isGalleryImport:
-                            true, // importing from the photo gallery
-                      ),
-                    ).scanDocument().then((value) async {
-                      Uint8List priviewImage =
-                          await File(value.images.elementAt(0)).readAsBytes();
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => CreatePdf(
-                                  list: value.images,
-                                  previewImage: priviewImage))));
-                    });
-                  },
-                ),
-                iconButtonWithText(
-                  icon: "assets/imges/removebg.png",
-                  text: 'image bg Remove',
-                  function: () async {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const PassportPhotoScreen();
-                    }));
-                  },
-                ),
-                iconButtonWithText(
-                  icon: "assets/imges/textRecognization.png",
-                  text: 'Text Recognization',
-                  function: () async {
-                    File? page;
-                    await DocumentScanner(
-                      options: DocumentScannerOptions(
-                        documentFormat:
-                            DocumentFormat.jpeg, // set output document format
-                        mode: ScannerMode
-                            .filter, // to control what features are enabled
-                        pageLimit:
-                            1, // setting a limit to the number of pages scanned
-                        isGalleryImport:
-                            true, // importing from the photo gallery
-                      ),
-                    ).scanDocument().then((value) async {
-                      for (var picture in value.images) {
-                        page ??= File(picture);
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return TextRecognization(
-                            selectedMedia: page!,
-                          );
-                        }));
-                      }
-                    });
-                  },
-                ),
-                iconButtonWithText(
-                  icon: "assets/imges/pdf.png",
-                  text: 'Merge PDF',
-                  function: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return MergePdf();
-                    }));
-                  },
-                ),
                 iconButtonWithText(
                   icon: "assets/imges/cvMaker.png",
                   text: 'Make CV',
