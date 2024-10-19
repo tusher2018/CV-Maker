d587fe2c66799d72ec51e5b965f86c486cef26bb
shishir2018 <www.shishir.symphony@gmail.com>
2024-10-19 23:01:32 +0600

    CV maker 1st push

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: CvTemplatesPage());
+    return MaterialApp(title: 'Flutter Demo', home: SplashScreen());
   }
 }
 
